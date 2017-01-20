ec9af656a8a7f92342986d10a062dfcf9aa179c0
emesonsantana <emesonsantana@gmail.com>
2017-01-20 11:27:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,6 @@ var FloatingLabel  = React.createClass({
         value: this.props.value,
         numberOfLines: this.props.numberOfLines,
         returnKeyLabel: this.props.returnKeyLabel,
-        clearTextOnFocus: this.props.clearTextOnFocus,
         dataDetectorTypes: this.props.dataDetectorTypes,
         enablesReturnKeyAutomatically: this.props.enablesReturnKeyAutomatically,
         keyboardAppearance: this.props.keyboardAppearance,
